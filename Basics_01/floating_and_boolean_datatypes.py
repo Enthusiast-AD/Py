914d926b9d9770d3a1b69ae3e16273b00a77f142
ANSH DEEP <ad0811@srmist.edu.in>
2025-02-05 09:31:46 +0000

    Basics of floating and boolean datatypes

---
+++ Basics_01/floating_and_boolean_datatypes.py
@@ -0,0 +1,9 @@
+# pi = float(3.14)
+pi = 3.14
+# print(pi)
+# print(type(pi))
+
+bool_example = bool(1)
+# bool_example = True
+print(bool_example) # It will give True as output
+print(type(bool_example))
