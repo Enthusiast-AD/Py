d2ceecd310ddecd1ed26cf9929af44e3ceabb531
ANSH DEEP <ad0811@srmist.edu.in>
2025-02-06 11:31:33 +0000

    stringing together variables

---
+++ Basics_02/stringing_together_variables.py
@@ -0,0 +1,9 @@
+name = "Yoda"
+print("Your name is "+name)
+height = 0.66
+age = 900
+
+# we need to change the float and int to str first to make them string together
+# The rule is that when you string together variables, they have to be 
+# classified as strings
+print("Your name is "+name+" , height is "+str(height)+" and your age is "+str(age)) 
