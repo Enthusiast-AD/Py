fa8f811d65550e971fc07d4859ceccd51d6a8674
ANSH DEEP <ad0811@srmist.edu.in>
2025-02-06 11:58:15 +0000

    basic built in functions complete

---
+++ Basics_03/basic_built_in_functions.py
@@ -0,0 +1,14 @@
+a = 1
+b = 2
+c = 3
+minimum_value = min(a,b,c)
+maximum_value = max(a,b,c)
+print(minimum_value)
+print(maximum_value)
+
+d = -7.6
+print(abs(d)) # abs-> used for absolute value meaning -ve value beocomes +ve
+
+print(round(d)) # this function rounds of the number
+# If we wanted to round the number to 2 decimal points, we would type this: a = round(a, 2)
+ 
