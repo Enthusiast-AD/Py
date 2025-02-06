05e3b956185b0910defcb0ad971a890d96c15f2f
ANSH DEEP <ad0811@srmist.edu.in>
2025-02-06 12:04:41 +0000

    exercise 4.1 complete

---
+++ Basics_03/ex_4.1.py
@@ -0,0 +1,9 @@
+ahsoka_height = 1.70
+yoda_height = 0.66
+r2d2_height = 1.09
+
+average_height = (ahsoka_height+yoda_height+r2d2_height)/3
+maximum_height = max(ahsoka_height,yoda_height,r2d2_height)
+minimum_height = min(ahsoka_height,yoda_height,r2d2_height)
+
+print("The average height is "+str(average_height)+". The maximum height is "+str(maximum_height)+". The minimum height is "+str(minimum_height))
